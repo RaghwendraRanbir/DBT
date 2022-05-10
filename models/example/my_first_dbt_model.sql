cf00b4f89b863aa7211ec12d81650102933f4d2b
raghwendraranbir5@gmail.com <raghwendraranbir5@gmail.com>
2022-05-10 11:59:12 +0000

    var

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -5,9 +5,10 @@
     This will override configurations stated in dbt_project.yml
 
     Try changing "table" to "view" below
+    we cant create database from dbt, just create and grant in snowflakes
 */
 
-{{ config(materialized='table') }}
+{{ config(materialized='table',alias='First_model',schema='ranbir_test',database='analytics_test') }}
 
 with source_data as (
 
@@ -17,7 +18,7 @@ with source_data as (
 
 )
 
-select *
+select *,{{var('my_first_var')}} as firt_var
 from source_data
 
 /*
